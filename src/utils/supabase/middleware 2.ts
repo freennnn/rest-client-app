13e174e7e851ccc5dcf1373aa8a31338101b2b5c
freen <freennnn@gmail.com>
2025-04-02 14:07:33 +0200

    feature: add eslint, husky, prettier, lint-staged, supabase client and ssr packages

---
+++ src/utils/supabase/middleware 2.ts
@@ -0,0 +1,51 @@
+import { createServerClient } from '@supabase/ssr';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value);
+          });
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) => {
+            supabaseResponse.cookies.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+
+  // Potential workaround for supabase.auth.getUser() being too slow for every request
+  // const { data: { session } } = await supabase.auth.getSession();
+  // if (session) {
+  //   supabaseResponse.cookies.set("sb-session", session.access_token, {
+
+  //   })
+  // }
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth')
+  ) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return supabaseResponse;
+}
