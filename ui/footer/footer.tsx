add1a18d40376ffd813a2291739b4996ea1651a7
Magadanov <magadanov03@mail.ru>
2025-04-07 22:49:50 +0500

    feat: implemented footer

---
+++ ui/footer/footer.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import rsLogo from '@/public/rs-logo.svg'
+
+export const Footer = () => {
+
+    return <footer className="bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 py-3 px-6">
+    <div className="container mx-auto flex flex-wrap justify-center md:justify-between items-center gap-4">
+      <div className="flex items-center gap-4">
+            <Link 
+                href="https://rs.school/courses/reactjs" 
+                target="_blank"
+                className="flex gap-2 align-center text-sm hover:underline hover:text-blue-600 dark:hover:text-blue-400"
+            >
+                <Image src={rsLogo} alt='rslogo' width={16} height={16}/>
+                RSschool
+            </Link>        
+        <span className="text-sm text-gray-600 dark:text-gray-300">© 2025 REST Client</span>
+      </div>
+      <div className='flex gap-4 align-center justify-center text-sm text-gray-600 dark:text-gray-300'>
+        <Link href={"https://github.com/freennnn"} target='_blank' className='hover:underline hover:text-blue-600 dark:hover:text-blue-400'>
+            @freennnn
+        </Link>
+        <Link href={"https://github.com/RUBBOSS"} target='_blank' className='hover:underline hover:text-blue-600 dark:hover:text-blue-400'>
+            @RUBBOSS
+        </Link>
+        <Link href={"https://github.com/magadanov"} target='_blank' className='hover:underline hover:text-blue-600 dark:hover:text-blue-400'>
+            @magadanov
+        </Link>
+      </div>
+      <div className="text-sm text-gray-600 dark:text-gray-300">
+        Built with Next.js and TailwindCSS
+      </div>
+    </div>
+  </footer>
+}
